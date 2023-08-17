748a532dcd3e3022283cfb41a6cbdf29d272a7da
xanderjakeq <13279523+xanderjakeq@users.noreply.github.com>
2023-08-17 16:29:59 -0400

    initialize webapp

---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<iframe
+    title="stream"
+	src="https://player.twitch.tv/?channel=xanderjakeq&muted=true&parent=localhost"
+	parent="localhost"
+	allowfullscreen
+/>
+
+<div id="chat-container">
+	<iframe
+        title="stream"
+		src="https://www.twitch.tv/embed/xanderjakeq/chat?parent=localhost&darkoutput"
+		parent="localhost"
+		height="<height>"
+		width="<width>"
+		id="chat"
+	/>
+</div>
+
+<style>
+	iframe {
+		width: 80vw;
+		height: 70vh;
+	}
+	#chat-container {
+		width: 20vw;
+		height: 50vh;
+	}
+</style>
