b7c0e04ccb30635e6e23e061d9e2a02205d28f51
xanderjakeq <13279523+xanderjakeq@users.noreply.github.com>
2023-08-18 23:57:17 -0400

    embed layout

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,31 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<iframe
-    title="stream"
-	src="https://player.twitch.tv/?channel=xanderjakeq&muted=true&parent=localhost"
-	parent="localhost"
-	allowfullscreen
-/>
-
-<div id="chat-container">
+<div
+	class="
+    flex align-center m-5
+    h-[70vh]
+    "
+>
 	<iframe
-        title="stream"
-		src="https://www.twitch.tv/embed/xanderjakeq/chat?parent=localhost&darkoutput"
+		title="stream"
+		src="https://player.twitch.tv/?channel=xanderjakeq&muted=true&parent=localhost"
 		parent="localhost"
-		height="<height>"
-		width="<width>"
-		id="chat"
+		allowfullscreen
+		class="grow-[3]"
 	/>
+
+	<div
+		id="chat-container"
+		class="
+        overflow-hidden flex relative
+        "
+	>
+		<div
+			class="
+            absolute w-[100%] h-[100%]
+            opacity-0 hover:opacity-100
+            flex justify-center items-center
+            p-5
+            "
+		>
+			<span
+				class="
+                text-white z-10 pointer-events-none
+                "
+			>
+				use the custom chat input below to chat without logging in
+			</span>
+			<div
+				class="
+                absolute w-[100%] h-[100%] bg-black
+                opacity-0 hover:opacity-100 top-0
+                left-0 z-0
+                "
+			/>
+		</div>
+		<iframe
+			title="stream"
+			src="https://www.twitch.tv/embed/xanderjakeq/chat?parent=localhost"
+			parent="localhost"
+			id="chat"
+			class="
+            pointer-events-none
+            "
+		/>
+	</div>
+</div>
+<div
+	class="
+    "
+>
+	<input placeholder="ask the dev anything" />
 </div>
 
-<style>
-	iframe {
-		width: 80vw;
-		height: 70vh;
-	}
-	#chat-container {
-		width: 20vw;
-		height: 50vh;
-	}
+<style lang="postcss">
 </style>
