a3a0203f76647e16891e63adcffb86e47ae712e4
Empairim <titusfaulknertull@yahoo.com>
2023-08-21 19:58:56 -0400

    added some styling to ProfileCard

--- app/src/lib/components/ProfileCard.svelte
+++ app/src/lib/components/ProfileCard.svelte
@@ -24,8 +24,8 @@
 		if (userData) {
 			data = {
 				displayName: userData.display_name,
-				github: '',
-				website: userData.offline_image_url,
+				// github: userData.profile_image_url,
+				website: userData.profile_image_url,
 				bio: userData.description,
 				email: userData.email
 			};
@@ -33,10 +33,20 @@
 	});
 </script>
 
-<div>
+<div class="card rounded-md shadow-2xl bg-purple-500 hover:bg-purple-600 p-6">
 	<h2>{data.displayName}</h2>
 	<p>{data.bio}</p>
 	<p>{data.email}</p>
-	<p>{data.website}</p>
+	<img src={data.website} alt="profile image" />
 	<p>{data.github}</p>
 </div>
+
+<style>
+	.card {
+		transition: background-color 0.3s ease-in-out;
+	}
+
+	.card:hover {
+		background-color: #9427b0;
+	}
+</style>
