f6a090e31cd9b61711bbc545a65f419666e71fb2
Empairim <titusfaulknertull@yahoo.com>
2023-08-21 19:29:41 -0400

    added ProfileCard Component will style later

---
+++ app/src/lib/components/ItemList.svelte
@@ -0,0 +1,69 @@
+<!-- ProfileCard.svelte -->
+<script>
+	export let displayName = '';
+	export let github = '';
+	export let website = '';
+	export let bio = '';
+</script>
+
+<div class="bg-white shadow-md p-6 rounded-lg">
+	<h2 class="text-xl font-semibold mb-4">Profile Card</h2>
+
+	<div class="mb-4">
+		<p class="font-semibold">Display Name:</p>
+		<p>{displayName}</p>
+	</div>
+
+	<div class="mb-4">
+		<p class="font-semibold">Github:</p>
+		<p>{github}</p>
+	</div>
+
+	<div class="mb-4">
+		<p class="font-semibold">Website:</p>
+		<p>{website}</p>
+	</div>
+
+	<div class="mb-4">
+		<p class="font-semibold">Bio:</p>
+		<p>{bio}</p>
+	</div>
+
+	<div class="mb-4">
+		<p class="font-semibold">Chat Placeholder:</p>
+		<p>Ask me anything</p>
+	</div>
+</div>
+
+<div class="bg-white shadow-md p-6 rounded-lg">
+	<h2 class="text-xl font-semibold mb-4">Profile Card</h2>
+
+	<div class="mb-4">
+		<p class="font-semibold">Display Name:</p>
+		<p>{displayName}</p>
+	</div>
+
+	<div class="mb-4">
+		<p class="font-semibold">Github:</p>
+		<p>{github}</p>
+	</div>
+
+	<div class="mb-4">
+		<p class="font-semibold">Website:</p>
+		<p>{website}</p>
+	</div>
+
+	<div class="mb-4">
+		<p class="font-semibold">Bio:</p>
+		<p>{bio}</p>
+	</div>
+
+	<div class="mb-4">
+		<p class="font-semibold">Chat Placeholder:</p>
+		<p>Ask me anything</p>
+	</div>
+</div>
+
+<style>
+	/* Add Tailwind classes or custom styles here */
+</style>
--- app/src/lib/components/LandingPage.svelte
+++ app/src/lib/components/LandingPage.svelte
@@ -1,26 +1,34 @@
-<h1
-	class="
-    text-3xl
-    font-bold
-    "
->
-	trq
-</h1>
+<script>
+	import ProfileCard from './ProfileCard.svelte';
+	let isLoggedIn = false;
 
-<div>
-	<p>
-		Ever thought of using twitch to show off your coding skills? You might already be doing it. What
-		if you send a link to your stream in your job applications? How will you know recruiters visited
-		your stream?
-	</p>
+	// Other script logic for your LandingPage component
+</script>
 
-	<p>
-		trq (twitch recruit) lets you create a clean page of your twitch stream with relevant
-		information. If recruiters catch u live, they can chat directly without logging in.
-	</p>
+<div class="flex justify-center items-center min-h-screen bg-green-100">
+	<div class="max-w-xl p-8 bg-white shadow-lg rounded-lg">
+		<h1 class="text-3xl font-bold mb-4">trq</h1>
 
-	<p>
-		(this is just a random idea while in the shower. don’t know if this will help with your job
-		search, but we are here to find out xD)
-	</p>
+		<div>
+			<p>
+				Ever thought of using Twitch to show off your coding skills? You might already be doing it.
+				What if you send a link to your stream in your job applications? How will you know
+				recruiters visited your stream?
+			</p>
+
+			<p>
+				trq (Twitch Recruit) lets you create a clean page of your Twitch stream with relevant
+				information. If recruiters catch you live, they can chat directly without logging in.
+			</p>
+
+			<p>
+				(This is just a random idea while in the shower. Don’t know if this will help with your job
+				search, but we are here to find out xD)
+			</p>
+		</div>
+	</div>
 </div>
+
+<style>
+	/* Your landing page styles */
+</style>
