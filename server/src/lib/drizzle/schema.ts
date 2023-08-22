fb038c88982d52ce5fcd6ecfbe791ca79601530e
xanderjakeq <13279523+xanderjakeq@users.noreply.github.com>
2023-08-22 16:06:10 -0400

    fmt

--- server/src/lib/drizzle/schema.ts
+++ server/src/lib/drizzle/schema.ts
@@ -1,21 +1,26 @@
-import { sqliteTable, AnySQLiteColumn, foreignKey, text, integer, uniqueIndex } from "drizzle-orm/sqlite-core"
-import { sql } from "drizzle-orm"
+import {
+    AnySQLiteColumn,
+    foreignKey,
+    integer,
+    sqliteTable,
+    text,
+    uniqueIndex,
+} from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
 
-
-export const pages = sqliteTable("pages", {
-	id: text("id").primaryKey().notNull(),
-	authorId: integer("author_id").notNull().references(() => users.id),
-	note: text("note"),
-	createdAt: integer("created_at").notNull(),
+export const pages = sqliteTable('pages', {
+    id: text('id').primaryKey().notNull(),
+    authorId: integer('author_id').notNull().references(() => users.id),
+    note: text('note'),
+    createdAt: integer('created_at').notNull(),
 });
 
-export const users = sqliteTable("users", {
-	id: integer("id").primaryKey({ autoIncrement: true }).notNull(),
-	username: text("username").notNull(),
-	email: text("email").notNull(),
-},
-(table) => {
-	return {
-		idxName: uniqueIndex("idx_name").on(table.username),
-	}
+export const users = sqliteTable('users', {
+    id: integer('id').primaryKey({ autoIncrement: true }).notNull(),
+    username: text('username').notNull(),
+    email: text('email').notNull(),
+}, (table) => {
+    return {
+        idxName: uniqueIndex('idx_name').on(table.username),
+    };
 });
--- server/src/lib/env.ts
+++ server/src/lib/env.ts
@@ -1,3 +1,3 @@
 import { config } from 'dotenv';
 
-export default  config();
+export default config();
