e1df43d24d1f02186e9d5122b70dd2d3581ee571
xanderjakeq <13279523+xanderjakeq@users.noreply.github.com>
2023-08-22 16:19:56 -0400

    implement GET users: creates the user if not found

--- server/src/routers/v1/users.ts
+++ server/src/routers/v1/users.ts
@@ -1,14 +1,75 @@
-import { Context, Router } from 'oak';
+import { type Context, Router } from 'oak';
+import type { User } from '$lib/drizzle/types.ts';
+
+import { Status } from 'http_status';
+import { eq, sql } from 'drizzle-orm';
+
 import { db } from '$lib/db/db.ts';
 import { users } from '$lib/drizzle/schema.ts';
+import { respondWithData, respondWithError } from '$lib/utils.ts';
+import env from '$lib/env.ts';
 
 export const usersRouter = new Router();
 
 usersRouter
     .get('/', async (ctx: Context) => {
-        ctx.response.body = 'todo implements users route';
-        const result = await db.select().from(users).all();
+        const accessToken = ctx.request.headers.get('Authorization')?.split(
+            ' ',
+        )[1];
 
-        ctx.response.type = 'application/json';
-        ctx.response.body = result;
+        //TODO(@xanderjakeq): create a middleware to validate accessToken
+        const res = await fetch('https://id.twitch.tv/oauth2/validate', {
+            headers: {
+                Authorization: 'Bearer ' + accessToken,
+            },
+        });
+
+        const data = await res.json();
+
+        if (!accessToken || !res.ok) {
+            respondWithError(ctx, Status.Unauthorized, {
+                error: 'unauthorized',
+            });
+            return;
+        }
+
+        let user: User;
+        try {
+            const existingUser = await db.select().from(users).where(
+                eq(users.username, data.login),
+            ).get();
+
+            if (!existingUser) {
+                throw new Error('user not found');
+            }
+
+            user = existingUser;
+        } catch (err) {
+            const res = await fetch(
+                'https://api.twitch.tv/helix/users',
+                {
+                    headers: {
+                        Authorization: 'Bearer ' + accessToken,
+                        'Client-Id': env.TWITCH_CLIENT_ID,
+                    },
+                },
+            );
+
+            const { data: twitchData } = await res.json();
+
+            user = await db.insert(users).values({
+                username: data.login,
+                email: twitchData[0].email,
+            }).returning().get();
+        }
+
+        respondWithData<User>(
+            ctx,
+            Status.OK,
+            {
+                data: {
+                    user,
+                },
+            },
+        );
     });
