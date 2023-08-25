53ad104e373b8fa9c35b0290f0851dc3b4ba6e20
xanderjakeq <13279523+xanderjakeq@users.noreply.github.com>
2023-08-25 00:05:45 -0400

    clean up new page dialog

--- app/src/lib/components/NewPageDialog.svelte
+++ app/src/lib/components/NewPageDialog.svelte
@@ -1,46 +1,40 @@
-<script>
-	// import { navigate } from 'svelte-navigator';
-	import { v4 as uuidv4 } from 'uuid';
+<script lang="ts">
+	import { page as appPage } from '$app/stores';
 
-	let page = { username: '', notes: '' };
-	let shortUuid = '';
+	import short from 'short-uuid';
 
-	// async function savePage() {
-	// 	const pageId = uuidv4();
-	// 	const response = await fetch('/api/pages', {
-	// 		method: 'POST',
-	// 		headers: {
-	// 			'Content-Type': 'application/json'
-	// 		},
-	// 		body: JSON.stringify({ username: page.username, notes: page.notes, pageId })
-	// 	});
-	// 	if (response.ok) {
-	// 		shortUuid = pageId.slice(0, 8);
-	// 		alert(`Page created with ID: ${shortUuid}`);
-	// 		navigate(`/pages/${shortUuid}`);
-	// 	} else {
-	// 		alert('Failed to create page');
-	// 	}
-	// }
+	//TODO: add proper types
+	export let user, savePage: (a: typeof page) => void;
+
+	const translator = short();
+	const shortUuid = translator.new();
+	let page = { id: shortUuid, user, notes: '' };
+
+	//TODO: move to utils
+	function copyToClipboard(text: string) {
+		navigator.clipboard.writeText(text);
+	}
+
+	function handleSave() {
+		//TODO: add "copied to clipboard" popup
+		copyToClipboard(`${$appPage.url.host}/${shortUuid}`);
+		savePage(page);
+	}
 </script>
 
 <div
 	class=" flex-col bg-black border border-purple-500 rounded-lg shadow-lg p-4 float-left"
 	style="margin: 0 auto;"
 >
-	<h1 class="font-bold text-purple-500 text-lg mb-4">New Page</h1>
+	<h1 class="font-bold text-purple-500 text-lg mb-4">{shortUuid}</h1>
 	<div class="flex-col">
-		<h2 class="mb-2 text-purple-600">Username:</h2>
-		<input class="border p-2 bg-purple-300 mb-2" id="usernameInput" bind:value={page.username} />
 		<h3 class="mb-2 text-purple-500">Notes:</h3>
-		<textarea
-			class="border p-2 bg-purple-300 resize-none mb-2"
-			id="notesInput"
-			bind:value={page.notes}
-		/>
+		<textarea class="border p-2 resize-none mb-2" id="notesInput" bind:value={page.notes} />
 	</div>
-	<button class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
-		>Create Page</button
+	<button
+		class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+		on:click={handleSave}
 	>
-	<!-- on:click={savePage} add that to button when server rdy -->
+		Create
+	</button>
 </div>
